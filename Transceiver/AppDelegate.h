a4ea0eabbf5c68456266e3da061fda104477b4b5
何军 <deathewind@163.com>
2015-09-18 13:53:43 +0800

    no message

---
+++ Transceiver/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Transceiver
+//
+//  Created by 何军 on 18/9/15.
+//  Copyright (c) 2015年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Transceiver/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Transceiver
+//
+//  Created by 何军 on 18/9/15.
+//  Copyright (c) 2015年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
